0cf568c42163638b2e8d01c20aff274113934d4e
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-08-15 19:10:50 -0300

    chore: atualizacao bd seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,29 @@
-# db/seeds.rb
+user = User.find_or_create_by!(email: 'user@example.com') do |user|
+  user.password = 'password123'
+  user.password_confirmation = 'password123'
+end
 
-# Certifique-se de que a gem Devise está instalada e configurada corretamente.
+contacts_data = [
+  { name: 'buscaterra', email: 'contato@buscaterra.com.br' },
+  { name: 'agesbioactive', email: 'contato@agesbioactive.com' },
+  { name: 'codecoast', email: 'hello@codecoast.io' },
+  { name: 'controllepro', email: 'contato@controllepro.com.br' },
+  { name: 'cliqueretire', email: 'contato@cliqueretire.com.br' },
+  { name: 'computerscience', email: 'atendimento@computerscience.com.br' },
+  { name: 'genedeyeba', email: 'suporte@genedeyeba.com' },
+  { name: 'gatinfosec', email: 'contato@gatinfosec.com' },
+  { name: 'mixreality', email: 'contato@mixreality.com.br' },
+  { name: 'ivoservices', email: 'contato@ivoservices.com' },
+  { name: 'synergiaconsultoria', email: 'contato@synergiaconsultoria.com.br' },
+  { name: 'eicon', email: 'info@eicon.com.br' },
+  { name: 'kikker', email: 'contato@kikker.com.br' },
+  { name: 'kria', email: 'contato@kria.vc' },
+  { name: 'libercapital', email: 'contato@libercapital.com.br' },
+  { name: 'cashway', email: 'contato@cashway.io' }
+]
 
-puts "Creating a default user..."
+contacts_data.each do |contact_data|
+  Contact.find_or_create_by!(contact_data.merge(user: user))
+end
 
-User.create!(
-  email: 'user@example.com',
-  password: 'password123',
-  password_confirmation: 'password123'
-)
-
-puts "User created with email: 'user@example.com' and password: 'password123'"
+puts "BANCO SEED POPULADO COM SUCESSO!"
